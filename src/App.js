73cf1ad71f67fb1b9eb471b5ad60ae1c5f2081e1
pratikk2609 <pratikpawar2609@gmail.com>
2022-09-24 10:24:27 +0530

    add function

--- src/App.js
+++ src/App.js
@@ -2,21 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  function add(numb , numb2){
+    return numb+numb2;
+  }
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      pravin pawar
+        Pratik Pawar
+        {add(34,45)}
       </header>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -7,7 +7,11 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <div>
+   
+      <App/>
+      
+    </div>
   </React.StrictMode>
 );
 
