7cc16b609d608be5f71a271ee1f6fa09b7018387
pratikk2609 <pratikpawar2609@gmail.com>
2022-09-24 14:09:31 +0530

    first component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Greet} from './components/Greet'
+import Welcome from './components/Welcome'
 
-function App() {
-
-  function add(numb , numb2){
-    return numb+numb2;
+class App extends Component {
+  render(){
+    return(
+      <div classname ="App">
+        <Greet></Greet>
+        <Welcome />
+        </div>
+    )
   }
-  
-  return (
-    <div className="App">
-      <header className="App-header">
-      pravin pawar
-        Pratik Pawar
-        {add(34,45)}
-      </header>
-    </div>
-  );
 }
 
 export default App;
---
+++ src/components/Greet.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+//function Greet(){
+//      return <h1>Hello Pratik</h1>
+//}
+
+export const Greet = () => <h1>Hello Pratik </h1>
+
+export default Greet
