fbc897701d5b2fd245e785da7be143e320be8740
pratikk2609 <pratikpawar2609@gmail.com>
2022-09-24 19:23:21 +0530

    state and button

--- src/App.js
+++ src/App.js
@@ -4,14 +4,15 @@ import './App.css';
 import Greet from './components/Greet'
 import Welcome from './components/Welcome'
 import Hello from './components/Hello'
-
+import Message from './components/Message';
 class App extends Component {
   render(){
     return(
       <div classname ="App">
-        < Greet name="Dnyandeo"/>
-        < Greet name="Pravin"/>
-        < Greet name="Pradeep"/>
+        <Message />
+        {/* < Greet name="Dnyandeo"/> */}
+        {/* < Greet name="Pravin"/> */}
+        {/*< Greet name="Pradeep"/> */}
         {/* <Welcome /> */}
         {/* <Hello /> */} 
         </div>
