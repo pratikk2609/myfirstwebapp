368d3787cabefe50aa9819f452380f1bfee1a7bd
pratikk2609 <pratikpawar2609@gmail.com>
2022-09-29 12:55:42 +0530

    HTML added

--- src/components/Fruits.js
+++ src/components/Fruits.js
@@ -5,19 +5,55 @@ function Fruits() {
 
     return (
         <div>
+            <header>
+                <h1>NAMES OF FRUITS</h1>
+                <h3>-Apple</h3>
+                
+                <h3>-Banana</h3>
+                
+                <h3>-Watermelon</h3>
+                
+                <h3>-Pear</h3>
+                
+                <h3>-Mango</h3>
+    
+            </header>
+
+            <br />
+
+            <section>
+                <b>Advantages of Fruits</b>
+            </section>
+
+            <br />
+
+            <article>
+                <i>-Fruits are a great source of vitamins and minerals</i>
+                <br />
+                <i>-Lots and lots of fiber</i>
+                <br />
+                <i>-They are low-calorie and low-fat</i>
+                <br />
+                <i>-Fruits help you maintain good health</i>
+                <br />
+            </article>
+    
+            <a href="https://www.google.co.in/"> Google </a>
+            <hr />
+            <img src="https://stylesatlife.com/wp-content/uploads/2020/03/Different-Types-of-Fruits.jpg" alt="fruits" />
+    
             {names.map(id => {
-                if (id === "Banana")
-                    return <p>{id}</p>
-                else
-                    return <button style={{
-                        backgroundColor: 'blue',
-                        width: '100px',
-                        height: '40px'
-                    }}>{id}</button>
+                    return <b style={{
+                        backgroundColor: 'green',
+                        fontSize : '25px'
+
+                    }}>{id}</b>
             })
             }
         </div>
     )
+
+
 }
 
 export default Fruits
