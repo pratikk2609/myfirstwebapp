bcc5e0822234b25580d7a0970ac2afa2de335167
pratikk2609 <pratikpawar2609@gmail.com>
2022-10-07 12:47:59 +0530

    added event handler

--- src/components/EventBind.js
+++ src/components/EventBind.js
@@ -3,28 +3,56 @@ import React, { Component } from 'react'
 class EventBind extends Component {
 
     constructor(props) {
-        super(props)
-
+        super(props);
         this.state = {
-            message: 'Hello'
+            count: 1,
+            message: "hello"
         }
-    }
+    };
 
-    clickhandler() {
-        this.setstate({
-            message: 'Goodbye!'
-        })
-        console.log(this)
+    handleButtonClick = (event) => {
+
+        this.setState({ count: 2 })
+        this.setState({ message: "bye" })
+        //console.log(event);
     }
 
     render() {
+        console.log(this.state.count);
+
         return (
             <div>
                 <div>{this.state.message}</div>
-                <button onClick={this.clickhandler.bind(this)}>Click</button>
+                <button type="button" class="btn btn-primary m-3"
+                    onClick={() => { this.handleButtonClick() }}>Generate Code</button>
             </div>
-        )
+        );
     }
+    // constructor(props) {
+    //     super(props)
+    //     //this.onClick = this.onClick.bind(this)
+    //     this.clickhandler = this.clickhandler.bind(this);
+
+    //     this.state = {
+    //         message: 'Hello'
+    //     }
+    // }
+
+    // clickhandler() {
+    //     this.setstate({
+    //         message: 'Goodbye!'
+    //     })
+    //     console.log("pravin")
+    // }
+
+    // render() {
+    //     return (
+    //         <div>
+    //             <div>{this.state.message}</div>
+    //             <button onClick={()=>{this.clickhandler()}}>Click</button>
+    //         </div>
+    //     )
+    // }
 }
 
 export default EventBind
